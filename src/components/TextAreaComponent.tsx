cbe52b50400c1a9b037e2083bcc26a6449e28170
iaguinho1727 <iagossassen2@gmail.com>
2024-10-27 18:29:34 -0300

    :bug: fix: Label for inputs

--- src/components/TextAreaComponent.tsx
+++ src/components/TextAreaComponent.tsx
@@ -4,11 +4,11 @@ import { InputComponentProps, TextAreaComponentProps } from "@/types"
 
 export default function TextAreComponent({label,setState,value,placeholder,is_required}: TextAreaComponentProps)
 {
-    const random_id=String(new Date().getTime())
     return (
         <>
-            <label htmlFor={random_id}>{label}</label>
-            <textarea required={is_required} onChange={data=>setState(data.target.value)} value={value} className="form-control" id={random_id} rows={3} placeholder={placeholder}></textarea>
+            <label htmlFor={label}>{label}</label>
+            <textarea required={is_required} onChange={data=>setState(data.target.value)} value={value} className="form-control" 
+            id={label} rows={3} placeholder={placeholder}></textarea>
         </>
     )
 }
